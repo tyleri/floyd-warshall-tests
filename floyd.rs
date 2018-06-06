4b5531376723e881f3a90be5db3323065d4a38ac
Tyler Ishikawa <tyi3@cornell.edu>
2018-06-05 22:56:03 -0700

    run rustfmt

--- floyd.rs
+++ floyd.rs
@@ -2,22 +2,25 @@ use std::io;
 
 fn read_parse_line() -> Vec<usize> {
     let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).expect("could not read line");
+    io::stdin()
+        .read_line(&mut input_line)
+        .expect("could not read line");
 
-    let split_line = input_line.split_whitespace()
-                                   .map(|x| x.parse::<usize>().unwrap())
-                                   .collect();
+    let split_line = input_line
+        .split_whitespace()
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect();
 
     split_line
 }
 
 fn main() {
-    let mut split_line : Vec<usize>;
+    let mut split_line: Vec<usize>;
 
     split_line = read_parse_line();
     let (n, e) = (split_line[0], split_line[1]);
 
-    let mut edges : Vec<Vec<i32>> = Vec::with_capacity(n);
+    let mut edges: Vec<Vec<i32>> = Vec::with_capacity(n);
     for i in 0..n {
         edges.push(Vec::with_capacity(n));
         for _ in 0..n {
@@ -28,7 +31,7 @@ fn main() {
     for _ in 0..e {
         split_line = read_parse_line();
         let (n1, n2, w) = (split_line[0], split_line[1], split_line[2]);
-        edges[n1-1][n2-1] = w as i32;
+        edges[n1 - 1][n2 - 1] = w as i32;
     }
 
     for k in 0..n {
@@ -37,7 +40,9 @@ fn main() {
                 continue;
             }
             for j in 0..n {
-                if edges[k][j] != -1 && (edges[i][j] == -1 || edges[i][k] + edges[k][j] < edges[i][j]) {
+                if edges[k][j] != -1
+                    && (edges[i][j] == -1 || edges[i][k] + edges[k][j] < edges[i][j])
+                {
                     edges[i][j] = edges[i][k] + edges[k][j];
                 }
             }
@@ -50,6 +55,6 @@ fn main() {
     for _ in 0..trials {
         split_line = read_parse_line();
         let (start, end) = (split_line[0], split_line[1]);
-        println!("{}", edges[start-1][end-1]);
+        println!("{}", edges[start - 1][end - 1]);
     }
 }
